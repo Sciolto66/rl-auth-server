e5e047c15806253c3cd8934d93354b66d60b4996
ronlangeveld <ron66langeveld@gmail.com>
2024-05-04 07:36:49 +0200

    01-initial-project

---
+++ src/test/java/nl/rowendu/rlauthserver/RlAuthServerApplicationTests.java
@@ -0,0 +1,11 @@
+package nl.rowendu.rlauthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RlAuthServerApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
